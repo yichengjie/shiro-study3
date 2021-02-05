1ab0afa1ba1db4ed8f2d9f2cd6aa2d0e2bce04a5
易成杰 <626659321@qq.com>
2021-02-05 22:14:15 +0800

    保存

--- hello-world/src/test/java/com/yicj/study/HelloWorldTest.java
+++ hello-world/src/test/java/com/yicj/study/HelloWorldTest.java
@@ -1,5 +1,6 @@
 package com.yicj.study;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
@@ -9,19 +10,42 @@ import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.Arrays;
 
+@Slf4j
 public class HelloWorldTest {
 
     @Test
     public void testHelloworld() {
+        Subject subject = login("classpath:shiro.ini", "zhang", "123");
+        //6、退出
+        subject.logout();
+    }
+
+    @Test
+    public void testHasRole() {
+        Subject subject = login("classpath:shiro-role.ini", "zhang", "123");
+        //判断拥有角色：role1
+        Assert.assertTrue(subject.hasRole("role1"));
+        //判断拥有角色：role1 and role2
+        Assert.assertTrue(subject.hasAllRoles(Arrays.asList("role1", "role2")));
+        //判断拥有角色：role1 and role2 and !role3
+        boolean[] result = subject.hasRoles(Arrays.asList("role1", "role2", "role3"));
+        Assert.assertEquals(true, result[0]);
+        Assert.assertEquals(true, result[1]);
+        Assert.assertEquals(false, result[2]);
+    }
+
+
+    private Subject login(String location, String username, String password){
         //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
-        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory(location);
         //2、得到SecurityManager实例 并绑定给SecurityUtils
         SecurityManager securityManager = factory.getInstance();
         SecurityUtils.setSecurityManager(securityManager);
         //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
         Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
         try {
             //4、登录，即身份验证
             subject.login(token);
@@ -29,7 +53,8 @@ public class HelloWorldTest {
             //5、身份验证失败
         }
         Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
-        //6、退出
-        subject.logout();
+        return subject ;
     }
+
+
 }
