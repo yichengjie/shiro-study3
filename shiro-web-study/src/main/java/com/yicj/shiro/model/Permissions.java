6f1e966dd3d83d454e0b4ff665736fe904ac6f7f
易成杰 <626659321@qq.com>
2021-02-14 12:51:16 +0800

    保存

---
+++ shiro-web-study/src/main/java/com/yicj/shiro/model/Permissions.java
@@ -0,0 +1,11 @@
+package com.yicj.shiro.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Permissions {
+    private String id;
+    private String permissionsName;
+}
---
+++ shiro-web-study/src/main/java/com/yicj/shiro/service/LoginService.java
@@ -0,0 +1,7 @@
+package com.yicj.shiro.service;
+
+import com.yicj.shiro.model.User;
+
+public interface LoginService {
+    User getUserByName(String username) ;
+}
